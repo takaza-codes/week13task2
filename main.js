aa87fd955b4dc64804ed0121e13ce7f5318f8990
takaza-codes <elekovalenko@gmail.com>
2025-01-19 21:01:36 +0300

    fix function for corner cases

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
 const inputDate = document.getElementById('userdate');
-const result = document.getElementById('result');
 const resultLine = document.querySelector(".resultLine");
 
 function calculateDaysUntil() {
     const dateEntered = inputDate.value;
+
+    if (!dateEntered) {
+        resultLine.textContent = 'Пожалуйста, введите дату';
+        resultLine.classList.add("error");
+        return;
+    }
+
     const dateEnteredTs = Date.parse(dateEntered);
     const currentTs = Date.now();
     const dateDifference = dateEnteredTs - currentTs;
     const daysUntil = Math.ceil(dateDifference / (1000 * 60 * 60 * 24));
 
+    let daysForm = 'дней';
+    let verbForm = 'осталось';
+
     if (daysUntil === 1) {
-        result.textContent = `остался всего ${daysUntil} день!`
+        verbForm = 'осталcя всего';
+        daysForm = 'день';
+    } else if (daysUntil % 100 >= 11 && daysUntil % 100 <= 14) {
+        daysForm = 'дней';
+    } else if (daysUntil % 10 === 1) {
+        verbForm = 'осталcя';
+        daysForm = 'день';
+    } else if (daysUntil % 10 >= 2 && daysUntil % 10 <= 4) {
+        verbForm = 'осталось всего';
+        daysForm = 'дня';
     }
-    else if (daysUntil === 2 || daysUntil === 3 || daysUntil === 4){
-        result.textContent = `осталось всего ${daysUntil} дня!`
-    } 
 
-    //чтобы не возникало иллюзий, что я такая умная, full disclosure: два else-if блока ниже сделаны по нагугленному образцу
-    else if (daysUntil % 10 === 1) {
-        result.textContent = `остался ${daysUntil} день!`
-        }
-
-    else if (daysUntil % 10 === 2 || daysUntil % 10 === 3 || daysUntil % 10 === 4) {
-        if (!(daysUntil >= 11 && daysUntil <= 14)) {
-            result.textContent = `осталось ${daysUntil} дня!`;
-            return;
-        }
-    } 
-
-    else {
-        result.textContent = `осталось ${daysUntil} дней!`
-    }
+    resultLine.textContent = `До вашего дня рождения ${verbForm} ${daysUntil} ${daysForm}!`;
+    resultLine.classList.remove("error");
 }
